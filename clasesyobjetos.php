3e7c0949ec34c42054304791035c3c7456b7ab5f
Felipe <felip_in_1@icloud.com>
2017-03-20 00:29:58 -0300

    iniciamos el proyecto

---
+++ clasesyobjetos.php
@@ -0,0 +1,18 @@
+<?php
+
+    class Persona{
+
+        //atributos
+        public $nombre = "pedro";
+
+        //metodos
+        public function hablar(){
+
+        }
+
+    }
+
+    $persona = new Persona();
+    $persona-> nombre
+
+?>
