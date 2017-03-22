341530848653e16239bf9d124be3e27dee389b60
Felipe <felip_in_1@icloud.com>
2017-03-21 22:01:05 -0300

    agregamos los atributos al metodo guardar

---
+++ metodosyatributos.php
@@ -0,0 +1,22 @@
+<?php 
+    class Persona 
+    {
+        //atributos
+        public $nombre;
+        public $apellido;
+
+        //metodos
+        public function guardar($nombre, $apellido){
+
+        }
+
+        public function mostrar(){
+
+        }
+    }
+
+    $persona = new Persona();
+
+
+
+?>
