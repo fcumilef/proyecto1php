894285a0d51676a9cb31e27eb72cc1d254b3c97b
Felipe <felip_in_1@icloud.com>
2017-03-21 22:15:27 -0300

    se finaliza :D

--- metodosyatributos.php
+++ metodosyatributos.php
@@ -2,20 +2,31 @@
     class Persona 
     {
         //atributos
-        public $nombre;
-        public $apellido;
+        public $nombre = array();
+        public $apellido = array();
 
         //metodos
         public function guardar($nombre, $apellido){
-
+        $this->nombre[] = $nombre;
+        $this->apellido[] = $apellido;
         }
 
         public function mostrar(){
-
+            for($i = 0; $i < count($this->nombre); $i++){
+                self::formato($this->nombre[$i], $this->apellido[$i]);
+            }
         }
+
+        public function formato($nombre, $apellido){
+        echo "Nombre:" . $nombre . "| Apellido:" . $apellido . "<br>";
+        }
+
     }
 
     $persona = new Persona();
+    $persona->guardar("Carlos", "Fernandez");
+    $persona->guardar("Uriel", "Hernandez");
+    $persona->mostrar();
 
 
 
