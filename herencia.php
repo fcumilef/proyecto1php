b16cae21b8f5688390228f9039184e491c7f3c88
Felipe <felip_in_1@icloud.com>
2017-03-23 13:08:49 -0300

    Agregamos atributos y metodos a la clase

--- herencia.php
+++ herencia.php
@@ -1,7 +1,29 @@
 <?php
 
         class vehiculo{
-            
+
+            //Atributos
+
+            public $motor = false;
+            public $marca;
+            public $color;
+
+            //Metodos
+
+            public function estado(){
+                if($this->motor)
+                echo "El motor esta encendido<br>"
+                else
+                echo "El motor esta apagado<br>"
+            }
+
+            public function encender(){
+                if($this->motor){
+                    echo "Cuidado, el motor esta prendido<br>";
+                }else{
+                    echo "El motor ahora esta encendido<br>"
+                }
+            }
         }
 
 
