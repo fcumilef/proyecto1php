8b2eea8f99bb2f73123247c89a42c0a1628358ea
Felipe <felip_in_1@icloud.com>
2017-03-23 17:03:27 -0300

    se agrega metodo bienvenida y se muestra nombrey url

--- static.php
+++ static.php
@@ -4,13 +4,23 @@
 
         //Atributos
 
-        public $nombre;
-        public static $url;
+        public $nombre = "Codigo facilito";
+        public static $url = "www.codigofacilito.com";
 
+        //Metodos
+
+        public function bienvenida(){
+
+           echo "bienvenidos a <b>" . $this->nombre . "</b> la pagina es<b>" . Pagina::$url . "<b><br>";
+
+        }
+        
+        
 
     }
 
-
+$pagina = new Pagina();
+$pagina->bienvenida();
 
 
 
